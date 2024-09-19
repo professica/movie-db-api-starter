95c6208c14644baf4a433120ab5992fab9e68982
professica <110791055+professica@users.noreply.github.com>
2024-09-19 11:42:59 -0700

    Create script-starter.js

---
+++ script-starter.js
@@ -0,0 +1,59 @@
+function getPopularMovies(){
+    // the endpoint
+    // TO DO
+    // the place on the page where we'll display the movies
+    let popularMovies = document.getElementById("popular");
+    let imgUrl = "https://image.tmdb.org/t/p/w400";
+
+
+    // ajax time!
+    // create the object
+    // TO DO
+
+    // attach event handlers
+    // TO DO
+    // set the response type
+    // TO DO
+    // open the request
+    // TO DO
+
+    // send the request
+    // TO DO
+}
+
+function getBirthYearMovies(e){
+    e.preventDefault();
+
+    // Get the user's input/year value
+    // TO DO
+    // the place on the page where we'll add the info
+    let birthYearMovies = document.getElementById("birthYear");
+
+    if(year < 1940 || year > 2024 || year == ""){
+        birthYearMovies.innerHTML = `<p style="color: red; background-color: white;">Please enter a year between 1940 and 2022</p>`;
+    }else{
+        // TO DO - Build the endpoint we need
+        // TO DO
+        let imgUrl = "https://image.tmdb.org/t/p/w400";
+
+        // ajax time!
+        // create the object
+        // TO DO
+
+        // attach event handlers
+        // TO DO
+        // set the response type
+        // TO DO
+        // open the request
+        // TO DO
+        // attach the headers (optional)
+
+        // send the request
+        // TO DO
+    }
+}
+
+window.addEventListener("load", function(){
+    getPopularMovies();
+    document.getElementById("yearBtn").addEventListener("click", getBirthYearMovies);
+});
